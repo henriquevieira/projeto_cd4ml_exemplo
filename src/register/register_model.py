ebd11563e8f66e6a664da347267888338e191fb5
Henrique Cursino Vieira <henrique.vieira@einstein.br>
2023-07-18 09:45:15 -0300

    fix(registry) Fix function

--- src/register/register_model.py
+++ src/register/register_model.py
@@ -17,7 +17,6 @@ class RegisterModel:
         self.algorithm_name = algorithm_name
         self.params_filepath = params_filepath
 
-        # self.remote_server_uri = "http://192.168.68.53:12000/" # this value has been replaced
         self.remote_server_uri = os.environ['MLFLOW_TRACKING_URL']
 
     def log_params(self, params):
@@ -68,7 +67,6 @@ class RegisterModel:
 
             trained_model = train_model.get_trained_model()
 
-            # predicted_qualities = lr.predict(test_x)
             y_pred = train_model.predict()
             X_train = train_model.get_X_train()
             y_test = train_model.get_y_test()
@@ -76,39 +74,16 @@ class RegisterModel:
             params  = train_model.get_params()
             metrics = train_model.eval_metrics(y_test, y_pred)
 
-            # print("Elasticnet model (alpha={:f}, l1_ratio={:f}):".format(alpha, l1_ratio))
-            # print("  RMSE: %s" % rmse)
-            # print("  MAE: %s" % mae)
-            # print("  R2: %s" % r2)
-
-            # mlflow.log_param("n_estimators", n_estimators)
-            # mlflow.log_param("criterion", criterion)
-            # mlflow.log_param("random_state", random_state)
-            # mlflow.log_metric("accuracy", accuracy)
-            # mlflow.log_metric("f1", f1)
-            # mlflow.log_metric("roc_auc", roc_auc)
-
             self.log_params(params)
             self.log_metrics(metrics)
 
             signature = infer_signature(X_train, y_test)
-
-            # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
-
-            # Model registry does not work with file store
-            # if tracking_url_type_store != "file":
-            #     # Register the model
-            #     # There are other ways to use the Model Registry, which depends on the use case,
-            #     # please refer to the doc for more information:
-            #     # https://mlflow.org/docs/latest/model-registry.html#api-workflow
             
             # TODO REMOVE HARDCODE
             mlflow.sklearn.log_model(trained_model, 
                                     "model",
                                     registered_model_name="RandomForestClassifierBreastCancerModel",
                                     signature=signature)
-            # else:
-            # mlflow.sklearn.log_model(classifier, "model") #, signature=signature)
 
             mlflow.log_artifacts("data", artifact_path="data")
             self.set_complement_info(run=run)
